78d898fed9e0b01eb43a6ad637736f7994debcf4
Ryan <pyan4444@gmail.com>
2023-05-27 22:06:27 -0400

    Made a bunch of useful library functions

--- src/minifier.rs
+++ src/minifier.rs
@@ -1,4 +1,4 @@
-use lua_parser::parser::ParserResult;
+use lua_parser::{parser::ParserResult, state::State, Result};
 
 use self::{statements::statement_list_minification, variables::load_locals};
 
@@ -18,4 +18,11 @@ pub fn minify_ast(parser_result: ParserResult) -> String {
     load_locals(parser_result.locals);
 
     statement_list_minification(&parser_result.ast)
+}
+
+pub fn minify(file: &str) -> Result<String> {
+    let mut state = State::new();
+    let parser_result = state.parse_file(file)?;
+
+    Ok(minify_ast(parser_result))
 }
