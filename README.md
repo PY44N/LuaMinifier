2b19d3598de844d316699573a86325d85b6a1fb9
Ryan <pyan4444@gmail.com>
2023-05-27 22:13:06 -0400

    Improved the readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Lua Minfier
 A basic lua minification program written in Rust
+
+## Usage
+
+Add the following to your *Cargo.toml* file add the depdendancy to your project
+```toml
+lua_minifier = { git = "https://github.com/PY44N/LuaMinifier" }
+```
+
+Use the following code to minify lua code from a file
+
+```rust
+let output = minify("file.lua").expect("Failed to minify");
+println!("{}", output);
+```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use std::{fs, path::Path};
 
-use lua_minifier::minifier::minify_ast;
+use lua_minifier::minifier::minify;
 use lua_parser::state::State;
 
 fn main() {
-    let mut state = State::new();
-    let parser_result = state.parse_file("input.lua").expect("Failed to parse file");
-
-    let output = minify_ast(parser_result);
+    let output = minify("input.lua").expect("Failed to minify");
     println!("{}", output);
     if Path::new("output.lua").exists() {
         fs::remove_file("output.lua").unwrap();
